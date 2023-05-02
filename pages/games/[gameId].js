336d2ba5f65ae3949d4eb29da49b7c1ab9920c3e
Harsh Mathur <harsh@felicity.care>
2023-05-02 16:23:22 +0530

    it doesn't work

--- pages/games/[gameId].js
+++ pages/games/[gameId].js
@@ -95,23 +95,23 @@ const Game = () => {
 
                 <div className="flex flex-col my-4">
                     <div className="flex flex-row">
-                        <div onClick={() => setFragment(1)} className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${fragment == 1 && 'border-b-2'}`}>
+                        <div className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${fragment == 1 && 'border-b-2'}`}>
                             <p className="font-Sora font-medium text-base">About</p>
                         </div>
-                        {(hasAccess) && <div onClick={() => setFragment(2)} className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${(fragment == 2) && 'border-b-2'}`}>
+                        {(hasAccess) && <div className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${(fragment == 2) && 'border-b-2'}`}>
                             <p className="font-Sora font-medium text-base">Announcements</p>
                         </div>}
-                        <div onClick={() => setFragment(3)} className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${fragment == 3 && 'border-b-2'}`}>
+                        <div className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${fragment == 3 && 'border-b-2'}`}>
                             <p className="font-Sora font-medium text-base">Collections</p>
                         </div>
-                        {hasAccess && <div onClick={() => setFragment(4)} className={`flex flex-row cursor-pointer justify-center border-solid border-[#00FFC2] ${fragment == 4 && 'border-b-2'} pl-5 pr-3 py-3`}>
+                        {hasAccess && <div className={`flex flex-row cursor-pointer justify-center border-solid border-[#00FFC2] ${fragment == 4 && 'border-b-2'} pl-5 pr-3 py-3`}>
                             <p className="font-Sora font-medium text-base">Livestreams</p>
                             <span className="relative flex h-2 w-2 ml-1">
                                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#62ff00c4] opacity-75"></span>
                                 <span className="relative inline-flex rounded-full h-2 w-2 bg-[#61FF00]"></span>
                             </span>
                         </div>}
-                        <div onClick={() => setFragment(5)} className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${fragment == 5 && 'border-b-2'}`}>
+                        <div className={`flex flex-col cursor-pointer border-[#00FFC2] border-solid px-3 py-3 ${fragment == 5 && 'border-b-2'}`}>
                             <p className="font-Sora font-medium text-base">Reviews</p>
                         </div>
                     </div>
