ae0cdb1ec11af7765ff5fe3dca493dd29e491bd8
Harsh Mathur <harsh@felicity.care>
2023-05-04 13:18:45 +0530

    frontend fixes

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Dianomi</title>
+      </Head>
       <body className='bg-[#07050F] text-[#FAFAFA]'>
         <Main />
         <NextScript />
