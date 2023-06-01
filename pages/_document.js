a15b26c7947258d8962da4e95751ecdb3740a343
Harsh Mathur <harsh@felicity.care>
2023-06-01 14:32:08 +0530

    another attempt at fixing the font

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,3 @@
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
